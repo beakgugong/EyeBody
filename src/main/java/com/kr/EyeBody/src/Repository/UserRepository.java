303854c745bb4cfcd0df2de3da40552c4fa892a0
jinseok <qkrwlstjr0131@naver.com>
2023-08-24 15:30:47 +0900

    Revert "server bulid"
    This reverts commit 011364dd03d2e9e6c8dc23c44433941806654833.

--- src/main/java/com/kr/EyeBody/src/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kr.EyeBody.src.Repository;
-
-import com.kr.EyeBody.src.Entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users,Long> {
-
-}
